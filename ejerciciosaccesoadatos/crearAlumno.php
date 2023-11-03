ea1e6dc86e68d806e1178e78840e644aa6e829ae
andres <arozados@educa.madrid.org>
2023-11-03 14:19:27 +0100

    empezado proyecto de trimestre 1

--- ejerciciosaccesoadatos/crearAlumno.php
+++ ejerciciosaccesoadatos/crearAlumno.php
@@ -5,10 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        fieldset{
+            display:flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+        }
+    </style>
 </head>
 
 <body>
-    <form action="crearAlumno.php" method="post">
+    <form action="crearAlumno.php" method="get">
         <fieldset>
             <legend>Introducier en base de datos un nuevo usuario</legend>
             <label >nombre:</label>
@@ -18,8 +25,8 @@
             <label >telefono:</label>
             <input type="text" placeholder="123456789" name="telefono" required pattern="[0-9]*9" title="Debe introducir 9 numeros Ej:000435323"/>
             <label >correo:</label>
-            <input type="text" placeholder="correo" name="correo" value="<?=$correo?>" required pattern="^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$" title="Debe introducir un correo con @"/>
-            <input type="number" hidden name="entrado" value="1">
+            <input type="text" placeholder="correo" name="correo"  required pattern="^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$" title="Debe introducir un correo con @"/>
+            <input type="hidden" name="entrado" value="1">
             <button>Crear Usuario</button>
             
         </fieldset>    
@@ -27,12 +34,14 @@
 
 </body>
 <?php
+
+if(isset($_GET["entrado"])){
     include 'BBDD.php';
     
-    $nombre=$_POST["nombre"];
-    $telefono=$_POST["apellido"];
-    $apellido=$_POST["telefono"];
-    $correo=$_POST["correo"];
+    $nombre=$_GET["nombre"];
+    $telefono=$_GET["apellido"];
+    $apellido=$_GET["telefono"];
+    $correo=$_GET["correo"];
     $bd=new BBDD();
     $bd->insertarAlumno($nombre,$telefono,$apellido,$correo);
     $bd->closeConnection();
@@ -51,11 +60,6 @@
         </tr>
         </table>";
     
-    
-    
-        
-    
-    
-    
+}
 ?>
 </html>
