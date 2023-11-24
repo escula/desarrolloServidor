98a7f98d16b23d3370436c04270c406c82d6333c
Andres Rozados <arozados@educa.madrid.org>
2023-11-24 09:21:49 +0100

    añadida documentacion

--- chuletaExamen/formularios.php
+++ chuletaExamen/formularios.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+header('Location:index.php?message="valor"&segundaValor="valor2"')
+<form action="/action_page.php">
+  <input type="radio" id="html" name="fav_language" value="HTML">
+  <label for="html">HTML</label><br>
+  <input type="radio" id="css" name="fav_language" value="CSS">
+  <label for="css">CSS</label><br>
+  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+  <label for="javascript">JavaScript</label>
+    <input type="submit" value="Submit">
+</form>
+?>
